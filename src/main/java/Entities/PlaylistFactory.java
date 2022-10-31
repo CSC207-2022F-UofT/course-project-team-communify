d776941cd352898b334c0af502bae40eff2e7e55
NicholasMacasaet <nicholas.macasaet@mail.utoronto.ca>
2022-10-31 15:35:45 -0400

    modified playlist object and created playlist factory

--- src/main/java/Entities/PlaylistFactory.java
+++ src/main/java/Entities/PlaylistFactory.java
@@ -1,4 +1,7 @@
 package Entities;
 
 public class PlaylistFactory {
+    public Playlist createPlaylist(String name, User owner, boolean isPublic){
+        return new Playlist(name,owner,isPublic);
+    }
 }
