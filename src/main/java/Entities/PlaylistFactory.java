7cd366a3b9fcb107f0f4a0db6e65f3599c80d466
NicholasMacasaet <nicholas.macasaet@mail.utoronto.ca>
2022-10-31 15:52:12 -0400

    modified playlist factory per sarah's request

--- src/main/java/Entities/PlaylistFactory.java
+++ src/main/java/Entities/PlaylistFactory.java
@@ -2,6 +2,6 @@ package Entities;
 
 public class PlaylistFactory {
     public Playlist createPlaylist(String name, User owner, boolean isPublic){
-        return new Playlist(name,owner,isPublic);
+        return new Playlist(name, owner, isPublic);
     }
 }
