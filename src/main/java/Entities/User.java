889082f30ccb5e76714abd8ecfca3145c9fe14a9
manya-mittal <manyamittal614@gmail.com>
2022-10-31 15:22:06 -0400

    Make User Abstract Class

--- src/main/java/Entities/User.java
+++ src/main/java/Entities/User.java
@@ -1,4 +1,20 @@
 package Entities;
 
-public interface User {
+import java.util.ArrayList;
+import java.util.List;
+
+public abstract class User {
+    private String Username;
+    private String password;
+    List<Playlist> playlistList = new ArrayList<Playlist>();
+
+    public String getUsername() {
+        return Username;
+    }
+    public String getPassword() {
+        return password;
+    }
+    public List<Playlist> getPlaylistList() {
+        return playlistList;
+    }
 }
