89577b4264d86867ba2be8c33fd4979b51ad7880
manya-mittal <manyamittal614@gmail.com>
2022-10-31 15:56:33 -0400

    Remove PlayList List

--- src/main/java/Entities/User.java
+++ src/main/java/Entities/User.java
@@ -1,24 +1,17 @@
 package Entities;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public abstract class User {
     private final String username;
     private final String password;
-    private final List<Playlist> playlistList;
-    public User(String username, String password, ArrayList<Playlist> playlists){
+
+    public User(String username, String password){
         this.username = username;
         this.password = password;
-        this.playlistList = playlists;
     }
     public String getUsername() {
         return username;
     }
     public String getPassword() {
         return password;
-    }
-    public List<Playlist> getPlaylistList() {
-        return playlistList;
     }
 }
