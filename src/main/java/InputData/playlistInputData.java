334cf4b7907082dcff840fe284591b133c29beb1
rafaelgacesa <rafaelgacesa@gmail.com>
2022-10-31 17:17:48 -0400

    Completed assigned input/output data files

--- src/main/java/InputData/playlistInputData.java
+++ src/main/java/InputData/playlistInputData.java
@@ -1,4 +1,22 @@
 package InputData;
 
+import Entities.Song;
+
+import java.util.ArrayList;
+
 public class playlistInputData {
+    private final ArrayList<Song> songs;
+    private final String name;
+
+    public playlistInputData(ArrayList<Song> songs, String name){
+        this.name = name;
+        this.songs = songs;
+    }
+    public ArrayList<Song> getSongs() {
+        return songs;
+    }
+
+    public String getName() {
+        return name;
+    }
 }
--- src/main/java/OutputData/songOutputData.java
+++ src/main/java/OutputData/songOutputData.java
@@ -1,4 +1,15 @@
 package OutputData;
 
+import Entities.Song;
+
 public class songOutputData {
+    private final Song song;
+
+    public songOutputData(Song song){
+        this.song = song;
+    }
+
+    public Song getSong() {
+        return song;
+    }
 }
