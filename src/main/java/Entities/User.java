13adfcf9337455248d150246ffed8483f7237d30
manya-mittal <manyamittal614@gmail.com>
2022-10-31 15:39:14 -0400

    Add Constructor to abstract user class

--- src/main/java/Entities/User.java
+++ src/main/java/Entities/User.java
@@ -4,12 +4,16 @@ import java.util.ArrayList;
 import java.util.List;
 
 public abstract class User {
-    private String Username;
-    private String password;
-    List<Playlist> playlistList = new ArrayList<Playlist>();
-
+    private final String username;
+    private final String password;
+    private final List<Playlist> playlistList;
+    public User(String u, String p, ArrayList<Playlist> playlists){
+        this.password = p;
+        this.username = u;
+        this.playlistList = playlists;
+    }
     public String getUsername() {
-        return Username;
+        return username;
     }
     public String getPassword() {
         return password;
