6ca1654a7ef4c2c78d84aaa331b2a963bedadb28
NicholasMacasaet <nicholas.macasaet@mail.utoronto.ca>
2022-10-31 15:11:02 -0400

    Playlist entity added

--- src/main/java/Entities/Playlist.java
+++ src/main/java/Entities/Playlist.java
@@ -1,4 +1,39 @@
 package Entities;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+
 public class Playlist {
+    private String name;
+    private User owner;
+    private boolean isPublic;
+    private LinkedList<Song> songList;
+
+    public Playlist(String name, User owner, boolean isPublic, Song firstSong){
+        this.name = name;
+        this.owner = owner;
+        this.isPublic = isPublic;
+        this.songList.addFirst(firstSong);
+    }
+
+    public void setName(String name){
+        this.name = name;
+
+    }
+    public String getName(){
+        return this.name;
+    }
+    // no set owner because the owner is permenantly set upon intilization
+
+    public User getOwner(){
+        return this.owner;
+    }
+
+    public void setPrivacy(boolean privacy){
+        this.isPublic = privacy;
+    }
+
+    public boolean returnPrivacy(){
+        return this.isPublic;
+    }
 }
