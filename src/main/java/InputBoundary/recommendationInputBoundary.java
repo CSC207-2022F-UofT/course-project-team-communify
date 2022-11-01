18ce68d9aa3ecf7ff6e1d4ab7f3fa9ce3470ffc2
rafaelgacesa <rafaelgacesa@gmail.com>
2022-10-31 23:12:52 -0400

    - Added example database files for users and playlists - created the input and output boundaries of the use case which I am responsible for

--- src/main/java/InputBoundary/recommendationInputBoundary.java
+++ src/main/java/InputBoundary/recommendationInputBoundary.java
@@ -1,4 +1,15 @@
 package InputBoundary;
 
+import InputData.playlistInputData;
+
+/**
+ * Application Business Rules layer interface to be implemented by a Use Case
+ * to allow communication between use cases and UI input (dependency inversion).
+ */
 public interface recommendationInputBoundary {
+    /**
+     * Requests a recommendation on the selected Playlist.
+     * @param p the Playlist input data on which the recommendation is requested
+     */
+    public void recommendation(playlistInputData p);
 }
