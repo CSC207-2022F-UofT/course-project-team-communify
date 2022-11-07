17735f719363ec0df1457ae577fb5f3be8f88134
NicholasMacasaet <nicholas.macasaet@mail.utoronto.ca>
2022-11-07 15:58:00 -0500

    Moved createPlaylist's javadoc for song parameter to secondary overloaded constructor

--- src/main/java/UseCase/createPlaylist.java
+++ src/main/java/UseCase/createPlaylist.java
@@ -6,13 +6,13 @@ import InputBoundary.newPlaylistInputBoundary;
 
 public class createPlaylist implements newPlaylistInputBoundary {
 
-    /**
-     @param song will only be used if creating a nonempty playlist with one song
-     */
     public Playlist newPlaylist(String playlistName, User Owner, boolean privacy){
         return new Playlist(playlistName, Owner, privacy);
         //empty playlist creation
     }
+    /**
+     @param song will only be used if creating a nonempty playlist with one song
+     */
     public Playlist newPlaylist(String playlistName, User Owner, boolean privacy, Song song){
         return new Playlist(playlistName, Owner, privacy, song);
         //non-empty playlist creation
