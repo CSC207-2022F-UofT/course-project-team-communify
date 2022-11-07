5d4fdcfe51cf65baa7961ff14966d27c678befdf
manya-mittal <manyamittal614@gmail.com>
2022-11-07 16:23:56 -0500

    Create searchInputData

--- src/main/java/InputData/searchInputData.java
+++ src/main/java/InputData/searchInputData.java
@@ -1,4 +1,13 @@
 package InputData;
 
 public class searchInputData {
+    private final String searchText;
+
+   public searchInputData(String text){
+       this.searchText = text;
+   }
+
+    public String getSearchText() {
+        return searchText;
+    }
 }
