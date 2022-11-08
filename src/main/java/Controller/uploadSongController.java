7d6243a4bee09e9988e8dc3864c3ee80882024e4
clin1967 <clin1967@users.noreply.github.com>
2022-11-08 09:24:11 -0500

    Just put some rough skeletal stuff down, figured I might as well upload it too. Alot of stuff is regurgitated from the CRC Model doc. Also added uploadSongOutputData, but generally didn't really focus on this part.
    Main focus was on restructuring things to include a songDsData in light of the clean architecture violation @rafaelgacesa pointed out here. (https://github.com/CSC207-2022F-UofT/course-project-team-communify/pull/20#issuecomment-1302574894) Let me know if I misinterpreted anything.

--- src/main/java/Controller/uploadSongController.java
+++ src/main/java/Controller/uploadSongController.java
@@ -1,4 +1,11 @@
 package Controller;
+import InputBoundary.uploadSongInputBoundary;
 
 public class uploadSongController {
+
+    uploadSongInputBoundary uploadSongInteractor;
+    public void upload(uploadSongInputBoundary uploadSongInteractor){
+        //TODO
+    }
+
 }
--- src/main/java/UseCase/uploadSong.java
+++ src/main/java/UseCase/uploadSong.java
@@ -1,4 +1,22 @@
 package UseCase;
+import Database.songAccessInterface;
+import Entities.Song;
+import InputBoundary.uploadSongInputBoundary;
+import InputData.uploadSongInputData;
 
-public class uploadSong {
+public class uploadSong implements uploadSongInputBoundary{
+
+    songAccessInterface songAccessInterface;
+    public void uploadSong(uploadSongInputData uploadSongInputData) {
+        //TODO: Read file, pass to createSong who uses readMetadata.
+    }
+
+    public void readMetadata(){
+        // TODO: helper function.
+    }
+
+    public Song createSong(){
+        //TODO: helper function.
+        return null;
+    }
 }
