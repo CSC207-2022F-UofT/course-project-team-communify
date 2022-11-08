0239d299057a6a5de9031ba9b7cbd2409ba0adba
Christina Lin <44586776+chrstinalin@users.noreply.github.com>
2022-11-08 09:47:05 -0500

    Update uploadSong.java

--- src/main/java/UseCase/uploadSong.java
+++ src/main/java/UseCase/uploadSong.java
@@ -8,15 +8,6 @@ public class uploadSong implements uploadSongInputBoundary{
 
     songAccessInterface songAccessInterface;
     public void uploadSong(uploadSongInputData uploadSongInputData) {
-        //TODO: Read file, pass to createSong who uses readMetadata.
-    }
-
-    public void readMetadata(){
-        // TODO: helper function.
-    }
-
-    public Song createSong(){
-        //TODO: helper function.
-        return null;
+        //TODO
     }
 }
