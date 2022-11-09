73f3a013c2dd6cc8163ba8e0f53f471f1b378d3b
Rafael Gacesa <68714873+rafaelgacesa@users.noreply.github.com>
2022-11-09 12:37:13 -0500

    Update ArtistUser.java
    These are already stored in User

--- src/main/java/Entities/ArtistUser.java
+++ src/main/java/Entities/ArtistUser.java
@@ -4,8 +4,6 @@ import java.util.ArrayList;
 
 public class ArtistUser extends User{
     private String artistName;
-    private String username;
-    private String password;
 
     public ArtistUser(String artistName, String username, String password){
         super(username, password);
