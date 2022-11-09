23ca22951fc6ec16319a7292784869702b4e1e78
SiqiZhang6 <zsq595633502@gmail.com>
2022-11-09 18:35:56 -0500

    updated ArtistUser and combined the factories into one factory.

--- src/main/java/Entities/ArtistUserFactory.java
+++
@@ -1,9 +0,0 @@
-package Entities;
-
-public class ArtistUserFactory implements userFactory{
-
-    @Override
-    public User createUser(String artistName, String username, String password) {
-        return new ArtistUser(artistName, username, password);
-    }
-}
--- src/main/java/Entities/userFactory.java
+++ src/main/java/Entities/userFactory.java
@@ -1,5 +1,10 @@
 package Entities;
 
-public interface userFactory {
-    User createUser(String artistName, String username, String password);
+public class userFactory {
+    public User createRegularUser(String username, String password){
+        return new RegularUser(username, password);
+    }
+    public User createArtistUser(String artistName, String username, String password){
+        return new ArtistUser(artistName, username, password);
+    }
 }
