89dadedaccafa893310fbd0d0551658bbe5ebe57
manya-mittal <manyamittal614@gmail.com>
2022-11-09 21:40:25 -0500

    Create Search Use Case Interactor

--- src/main/java/UseCase/Search.java
+++ src/main/java/UseCase/Search.java
@@ -1,4 +1,39 @@
 package UseCase;
 
-public class Search {
+import Entities.Song;
+import InputBoundary.searchInputBoundary;
+import OutputBoundary.searchOutputBoundary;
+import Database.songAccessInterface;
+import InputData.searchInputData;
+import Database.songDsData;
+import OutputData.searchOutputData;
+
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+public class Search implements searchInputBoundary {
+    private searchOutputBoundary searchOutputBoundary;
+
+    songAccessInterface songLibrary;
+
+    public Search(songAccessInterface songLibrary, searchOutputBoundary searchOutputBoundary ){
+        this.songLibrary = songLibrary;
+        this.searchOutputBoundary = searchOutputBoundary;
+    }
+    public void search(searchInputData searchInputData){
+        String name = searchInputData.getSearchText();
+        Collection<songDsData> library = songLibrary.getLibrary();
+        List<Song> foundSongs = new ArrayList<Song>();
+
+        for (songDsData song: library) {
+            String currentName = song.getSong().getName();
+            if(currentName.equals(name)){
+                foundSongs.add(song.getSong());
+            }
+        }
+        searchOutputData outputData = new searchOutputData(foundSongs);
+        // TODO: call presenter's foundSong method
+    }
 }
