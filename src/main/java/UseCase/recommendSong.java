037dc2e684493c15eb6711da179083c5412be52f
rafaelgacesa <rafaelgacesa@gmail.com>
2022-11-11 13:59:58 -0500

    Skeleton of recommendSong use case.

--- src/main/java/UseCase/recommendSong.java
+++ src/main/java/UseCase/recommendSong.java
@@ -1,4 +1,42 @@
 package UseCase;
 
-public class recommendSong {
+import Database.songAccessInterface;
+import Entities.Song;
+import InputBoundary.recommendationInputBoundary;
+import InputData.playlistInputData;
+import OutputBoundary.recommendationOutputBoundary;
+import OutputData.songOutputData;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+/**
+ * Application Business Rules layer Use Case Interactor for generating a recommendation based on
+ * a given Playlist.
+ */
+public class recommendSong implements recommendationInputBoundary {
+    songAccessInterface library;
+    recommendationOutputBoundary out;
+
+    public recommendSong(songAccessInterface library, recommendationOutputBoundary out){
+        this.library = library;
+        this.out = out;
+    }
+
+    /**
+     * Generates a recommendation based on the given Playlist.
+     * @param p the Playlist input data on which the recommendation is requested
+     */
+    @Override
+    public void recommendation(playlistInputData p) {
+        Random random = new Random();
+        ArrayList<Song> playlist = p.getSongs();
+
+        Song randomSong = playlist.get(random.nextInt(playlist.size()));
+
+        // TODO: find another song by the genre of random song, implement once Song and songLibrary are implemented
+
+        Song rec = null;
+        out.recommendSong(new songOutputData(rec));
+    }
 }
