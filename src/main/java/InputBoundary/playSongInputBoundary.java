0dfd9fb91fc52473796af89af57e8400c851ef6b
Rafael Gacesa <68714873+rafaelgacesa@users.noreply.github.com>
2022-11-11 15:29:12 -0500

    Update playSongInputBoundary.java

--- src/main/java/InputBoundary/playSongInputBoundary.java
+++ src/main/java/InputBoundary/playSongInputBoundary.java
@@ -1,10 +1,7 @@
 package InputBoundary;
 
-import java.io.File;
 import InputData.songInputData;
 
 public interface playSongInputBoundary {
     public void play();
-
-    public static void playAudio(File song);
 }
