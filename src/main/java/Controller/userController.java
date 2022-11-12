3e1faa43cbe9a91ebfb544820791699192c155a0
SiqiZhang6 <zsq595633502@gmail.com>
2022-11-12 18:12:45 -0500

    Made the userController and merged the inputboundary for login and register (since they share the same data and boundary). The red in userController.java is because loginIB have not been implemented.

--- src/main/java/Controller/userController.java
+++ src/main/java/Controller/userController.java
@@ -1,4 +1,37 @@
 package Controller;
 
+import InputBoundary.loginInputBoundary;
+import InputData.loginInputData;
+
+
+/**
+ * Controller for login and register use cases.
+ */
 public class userController {
+    final loginInputBoundary loginInteractor, registerInteractor;
+    /**
+     * Constructor.
+     * @param loginInteractor use case interactor for adding a song to the space
+     * @param registerInteractor use case interactor for playing the space
+     */
+    public userController(loginInputBoundary loginInteractor, loginInputBoundary registerInteractor) {
+        this.loginInteractor = loginInteractor;
+        this.registerInteractor = registerInteractor;
+    }
+
+    /**
+     * function calling the use case for playing the space
+     * @param loginID input data for playing the space
+     */
+    public void login(loginInputData loginID){
+        this.loginInteractor.login(loginID);
+    }
+
+    /**
+     * function calling the use case for adding a song to the space
+     * @param registerID input data for adding a song to the space
+     */
+    public void register(loginInputData registerID){this.registerInteractor.register(registerID);
+    }
+   
 }
