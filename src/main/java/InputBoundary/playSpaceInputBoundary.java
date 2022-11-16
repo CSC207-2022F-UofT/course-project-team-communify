059c59f325b0f66c588bd51d79675c1fc793ad1c
Sarah <sarxhlwalker@gmail.com>
2022-11-15 20:13:43 -0500

    brackets

--- src/main/java/InputBoundary/playSpaceInputBoundary.java
+++ src/main/java/InputBoundary/playSpaceInputBoundary.java
@@ -12,7 +12,7 @@ public interface playSpaceInputBoundary {
      * function to play the space
      * @param songInputData data required to play the space
      */
-    default void playSpace(playSpaceInputData songInputData){}
+    void playSpace(playSpaceInputData songInputData);
 
     /**
      * function to play a random song given that the space controller has no songs queued
--- src/main/java/OutputBoundary/spacePlayedOutputBoundary.java
+++ src/main/java/OutputBoundary/spacePlayedOutputBoundary.java
@@ -1,6 +1,5 @@
 package OutputBoundary;
 
-import OutputData.songOutputData;
 
 /**
  * interface for wrapping the space presenter for the playing the space use case.
@@ -10,12 +9,11 @@ public interface spacePlayedOutputBoundary {
     /**
      * in case of starting to play space, reflect that in the button
      */
-    default void spacePlayed(){
-    }
+    void spacePlayed();
 
     /**
      * in case of starting to play a song/playlist when a space is currently playing, want to reset "play space!" button
      */
-    default void spaceNotPlayed(){}
+    void spaceNotPlayed();
 
 }
