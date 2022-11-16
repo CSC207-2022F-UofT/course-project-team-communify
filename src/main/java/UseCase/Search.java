1a029648f3ff562a2267d4f6a01fcabf00bbeafb
manya-mittal <manyamittal614@gmail.com>
2022-11-15 20:45:18 -0500

    Update search method to reduce code smells and improve functionality

--- src/main/java/UseCase/Search.java
+++ src/main/java/UseCase/Search.java
@@ -24,22 +24,38 @@ public class Search implements searchInputBoundary {
     }
 
     /**
-     *
+     * Method that goes through the database (songLibrary) to get the songs that match what was searched
      * @param searchInputData: contains the text to search for
      */
     @Override
     public void search(searchInputData searchInputData){
-        String name = searchInputData.getSearchText();
+        String name = searchInputData.getSearchText().toLowerCase();
         Collection<songDsData> library = songLibrary.getLibrary();
-        List<Song> foundSongs = new ArrayList<>();
-
-        for (songDsData song: library) {
-            String currentName = song.getSong().getName();
-            if(currentName.equals(name)){
-                foundSongs.add(song.getSong());
-            }
-        }
+        List<Song> foundSongs = findSongs(name, library);
         searchOutputData outputData = new searchOutputData(foundSongs);
         this.searchPresenter.foundSongs(outputData);
+    }
+
+    /**
+     * Helper function for search().
+     * Goes through database and finds songs that have an exact match or start with the name
+     * @param name
+     * @param library
+     * @return A List with the exact matches at the top and similar songs after
+     */
+    private static List<Song> findSongs(String name, Collection<songDsData> library) {
+        List<Song> foundSongs = new ArrayList<>();
+        List<Song> similarSongs = new ArrayList<>();
+
+        for (songDsData song: library) {
+            String currentName = song.getSong().getName().toLowerCase();
+            if(currentName.equals(name)){
+                foundSongs.add(song.getSong());
+            } else if (currentName.startsWith(name)){
+                similarSongs.add(song.getSong());
+            }
+        }
+        foundSongs.addAll(similarSongs);
+        return foundSongs;
     }
 }
