c04f6ed3d4e35d204b9405abe247f2ab014345e5
Mistryr222 <mistryr222@gmail.com>
2022-11-17 13:53:23 -0500

    Added Templates and functionalities to the artistView, loginRegisterView, and edited the formatting for playlistView.

--- src/main/java/View/artistView.java
+++ src/main/java/View/artistView.java
@@ -1,4 +1,52 @@
 package View;
 
-public class artistView {
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class artistView extends JFrame implements ActionListener {
+
+    private final int FONTSIZE = 10;
+    private final int WIDTH = 640;
+    private final int HEIGHT = 640;
+    private JFrame jframe;
+    private JButton uploadButton;
+
+
+    public artistView() {
+        this.initializeValues();
+        this.initializeComponents();
+        this.initializeFrame();
+    }
+
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        // if (e.getSource() == this.uploadButton) {}
+    }
+
+
+    private void initializeValues() {
+        this.jframe = new JFrame();
+        this.jframe.setSize(this.WIDTH, this.HEIGHT);
+        this.jframe.setResizable(false);
+        this.jframe.getContentPane().setBackground(new Color(185, 226, 246));
+        this.jframe.setLayout(null);
+        this.jframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+
+
+    private void initializeComponents() {
+        //this.uploadButton = new JButton();
+
+        //this.uploadButton.addActionListener(this);
+    }
+
+
+    private void initializeFrame() {
+        // this.jframe.add(uploadButton);
+        this.jframe.setVisible(true);
+    }
+
 }
--- src/main/java/View/playlistView.java
+++ src/main/java/View/playlistView.java
@@ -9,18 +9,19 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 /**
- * view for the user dashboard
+ * View for the user dashboard
  */
 public class playlistView extends JFrame implements ActionListener {
+
+    private final int FONTSIZE = 10;
+    private final int WIDTH = 640;
+    private final int HEIGHT = 640;
     private User user;
     private JFrame jframe;
     private JPanel panel;
     private JButton spaceButton;
     private JLabel title;
     private Font font;
-    private int fontSize = 10;
-    private int width = 640;
-    private int height = 640;
     private boolean spacePlaying;
     private ViewModel.musicEngineControllerViewModel musicEngineControllerViewModel;
 
@@ -29,26 +30,24 @@ public class playlistView extends JFrame implements ActionListener {
      * @param user takes in the user's data to display their own dashboard!
      */
     public playlistView(User user){
-        // initialize values and panels
-        this.initializeValues(user);
 
-        // set up space button
-        this.setSpaceButton();
+        this.initializeValues(user);
+        this.initializeComponents();            // set up space button
 
         // set up actual playlists
-        // TODO
         // TODO -- note: when starting to play a playlist, please update the spaceButtonText if it was being played
+
 
         // set up search bar
         // TODO
 
+
         // set up play bar
         // TODO
 
-        // set screen visible
-        this.setVisible();
-
+        this.initializeFrame();
     }
+
 
     /**
      * Invoked when an action occurs.
@@ -56,48 +55,62 @@ public class playlistView extends JFrame implements ActionListener {
      */
     @Override
     public void actionPerformed(ActionEvent e) {
+
         if (e.getSource() == this.spaceButton){
-            if (!this.spacePlaying){   // clicking on button when space is playing does nothing
+
+            if (!this.spacePlaying) {                          // clicking on button when space is playing does nothing
                 String message = this.musicEngineControllerViewModel.callPlaySpace();
                 this.spaceButton.setText(message);
+
             }
-        }
-        // TODO -- NOTE: add your action commands as an else-if to this if statement
+        } // TODO -- NOTE: add your action commands as an else-if to this if statement
+
     }
 
-    private void initializeValues(User user){
+
+    private void initializeValues(User user) {
+
         this.user = user;
-        this.jframe = new JFrame(this.user.getUsername() + " Dashboard");
+
+        this.jframe = new JFrame(this.user.getUsername() + "'s Dashboard");
+        this.jframe.setSize(this.WIDTH, this.HEIGHT);
+        this.jframe.setResizable(false);
+        this.jframe.getContentPane().setBackground(new Color(185, 226, 246));
+        this.jframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        // set up panel
+        // TODO: determine whether this should go in a helper function or not (i.e. how many panels do we want?)
         this.panel = new JPanel();
+        panel.setLayout(null);
+        panel.setBounds(0, 0, 10, 10);  // TODO - *Update*: For layout ask Rohan or Christina do not make a panel take up the entire screen size, these are just test dimensions to edit afterwards
+        panel.setBackground(Color.GREEN);
+
         this.spaceButton = new JButton("Listen to space!");
         this.title = new JLabel(this.user.getUsername() + " Dashboard");
-        this.font = new Font(title.getFont().getName(), Font.PLAIN, this.fontSize);
+        this.font = new Font(title.getFont().getName(), Font.PLAIN, this.FONTSIZE);
+
         // this.spaceViewModel = new spaceViewModel();
         this.musicEngineControllerViewModel = new musicEngineControllerViewModel();
         this.spacePlaying = false;
 
-        // set up panel
-        // TODO: determine whether this should go in a helper function or not (i.e. how many panels do we want?)
-        panel.setLayout(null);
-        panel.setBounds(0,0, this.width, this.height);  // TODO: probably want this to be larger
-        panel.setBackground(Color.BLUE);
     }
 
-    private void setSpaceButton(){
+
+    private void initializeComponents() {
         this.spaceButton.setBounds(350,30, 150,55);
-        this.spaceButton.setBackground(Color.GREEN);    // TODO: how to make actually green background
+        this.spaceButton.setFocusable(false);
+        this.spaceButton.setHorizontalTextPosition(JButton.CENTER);
+        this.spaceButton.setBackground(Color.white);
         this.spaceButton.setOpaque(true);
         this.spaceButton.setFont(font);
         this.spaceButton.addActionListener(this);
     }
 
-    private void setVisible(){
-        this.jframe.setSize(this.width, this.height);
-        this.jframe.setResizable(false);
+
+    private void initializeFrame() {
         this.panel.add(title);
         this.panel.add(this.spaceButton);
         this.jframe.add(panel);
-        this.jframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.jframe.setVisible(true);
     }
 }
