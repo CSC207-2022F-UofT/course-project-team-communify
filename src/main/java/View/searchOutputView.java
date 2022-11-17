415379cb903d620552f67b730907ecaae0e58baa
manya-mittal <manyamittal614@gmail.com>
2022-11-17 15:14:28 -0500

    Refactor to split set up into different functions to reduce code smells

--- src/main/java/View/searchOutputView.java
+++ src/main/java/View/searchOutputView.java
@@ -24,21 +24,29 @@ public class searchOutputView extends JFrame implements ActionListener {
 
     private JTable table;
     private JComboBox comboBox;
+    private String[][] data;
 
     private searchViewModel searchViewModel;
 
     public searchOutputView(){
         initialise();
+        this.setUpTable();
+        this.setVisible();
+    }
 
-        // set up panel
-//        panel.setLayout(null);
-//        panel.setBounds(0,0, this.width, this.height);  // TODO: probably want this to be larger
-//        panel.setBackground(Color.BLUE);
+    public void initialise(){
+        this.searchViewModel = new searchViewModel();
+        this.jframe = new JFrame("Search Results");
+        this.panel = new JPanel();
+        this.title = new JLabel("Search results for ");
+        this.font = new Font(title.getFont().getName(), Font.PLAIN, this.fontSize);
+    }
 
-        String[][] data = this.searchViewModel.search("stalin's head");
+    public void setUpTable(){
+        this.data = this.searchViewModel.search("fo");
 
         String[] columnNames = {"ID", "Name", "Artist", "Genre"};
-        table = new JTable(data, columnNames);
+        table = new JTable(this.data, columnNames);
         table.setBounds(30, 40, this.width, this.height);
         comboBox = new JComboBox();
         comboBox.addItem("Add to Space");
@@ -46,38 +54,25 @@ public class searchOutputView extends JFrame implements ActionListener {
         TableColumnModel columnModel = table.getColumnModel();
         columnModel.addColumn(new TableColumn());
 
-        for (int i = 0; i < data.length; i++) { // makes the default writing in 4th column a prompt
+        for (int i = 0; i < data.length; i++) { // makes the default writing in last column a prompt for combo box
             table.setValueAt("Add to..", i, 4);
         }
-
         columnModel.getColumn(4).setCellEditor(new DefaultCellEditor(comboBox));
         columnModel.removeColumn(table.getColumnModel().getColumn(0));
         comboBox.addActionListener(this);
         this.scrollPane = new JScrollPane(table);
         scrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
+    }
 
-
-
-
-
+    public void setVisible(){
         // set visible
         this.jframe.setSize(this.width, this.height);
-//        this.jframe.setResizable(false);
-        // this.panel.add(title);
+        this.jframe.setResizable(false);
         this.jframe.add(panel);
         this.jframe.add(scrollPane);
         this.jframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.jframe.setVisible(true);
 
-    }
-
-    public void initialise(){
-        this.searchViewModel = new searchViewModel();
-
-        this.jframe = new JFrame("Search Results");
-        this.panel = new JPanel();
-        this.title = new JLabel("Search results for ");
-        this.font = new Font(title.getFont().getName(), Font.PLAIN, this.fontSize);
     }
 
     /**
