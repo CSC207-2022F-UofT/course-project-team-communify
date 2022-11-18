42326424ed099321b08770baf821f4d593d74418
Christina Lin <44586776+chrstinalin@users.noreply.github.com>
2022-11-17 20:31:50 -0500

    Update uploadSongInputData.java

--- src/main/java/InputData/uploadSongInputData.java
+++ src/main/java/InputData/uploadSongInputData.java
@@ -7,11 +7,17 @@ public class uploadSongInputData {
         this.filepath = filepath;
         this.uploader = uploader;
     }
-
+    
+    /**
+    * @return filepath of the song to be uploaded
+    */
     public String getFilepath(){
         return this.filepath;
     }
 
+    /**
+    * @return username of the artist who uploaded the song
+    */
     public String getUploader(){
         return this.uploader;
     }
