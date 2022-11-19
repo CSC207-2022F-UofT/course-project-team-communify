39da08ae2d62f2f273d160f2099f6940c746b7d6
Christina Lin <44586776+chrstinalin@users.noreply.github.com>
2022-11-18 22:35:24 -0500

    Update songLibrary.java

--- src/main/java/Database/songLibrary.java
+++ src/main/java/Database/songLibrary.java
@@ -131,7 +131,7 @@ public class songLibrary implements SaveSongAccessInterface, GetSongAccessInterf
     private songDsData readSongFromMetadata(int id, String uploader, MP3File rawSong){
 
         Tag tag = rawSong.getTag();
-        BufferedImage cover = ImageIO.read(new File("src/songLib/cover/no_genre.png"));
+        BufferedImage cover = ImageIO.read(new File("./src/songLib/cover/no_genre.png"));
         Artwork rawCover = tag.getFirstArtwork();
         if(rawCover != null) cover = (BufferedImage) rawCover.getImage();
         
