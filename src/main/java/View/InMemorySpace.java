16f470c49cf717f09282d10d06a1b5d254ede5c2
Sarah <sarxhlwalker@gmail.com>
2022-11-21 14:40:45 -0500

    Revert "create inmemoryspace"
    This reverts commit 55bce14272e1550a98f6137996f33cc9bf509f6f.

--- src/main/java/View/InMemorySpace.java
+++ src/main/java/View/InMemorySpace.java
@@ -1,25 +1,2 @@
-package View;
-
-import ViewModel.musicEngineControllerViewModel;
-
-import java.util.ArrayList;
-import java.util.List;
-
-public class InMemorySpace {
-
-    private final List<InMemorySong> spaceSongList;
-    private final musicEngineControllerViewModel musicEngineControllerViewModel;
-
-    public InMemorySpace(musicEngineControllerViewModel musicEngineControllerViewModel){
-        this.spaceSongList = new ArrayList<>();
-        this.musicEngineControllerViewModel = musicEngineControllerViewModel;
-    }
-
-    public List<InMemorySong> getSpaceSongList(){
-        return this.spaceSongList;
-    }
-
-    public musicEngineControllerViewModel getMusicEngineControllerViewModel(){
-        return this.musicEngineControllerViewModel;
-    }
+package View;public class InMemorySpace {
 }
