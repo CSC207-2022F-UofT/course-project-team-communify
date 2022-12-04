e91cc449bda8683fee541babc0823f622b9f0e9c
clin1967 <clin1967@users.noreply.github.com>
2022-12-04 18:17:20 -0500

    Javadocs + protected

--- src/test/java/viewModel/ArtistViewModelTest.java
+++ src/test/java/viewModel/ArtistViewModelTest.java
@@ -8,18 +8,25 @@ public class ArtistViewModelTest {
 
     ArtistViewModel avm = new ArtistViewModel(new InMemoryArtistUser("Metrofolk", "metrofolk"));
 
+    /**
+     * Tests whether a Song is successfully added to UploadQueue when upload in ArtistViewModel is called.
+     */
     @Test
     public void testUpload(){
-        // Song is successfully added to UploadQueue.
         Assertions.assertTrue(avm.upload("./src/test/java/viewModel/test.mp3"));
     }
 
+    /**
+     * Tests that songs are successfully retrieved by getArtistSong
+     */
     @Test
     public void testGetArtistSongs(){
-        // Song is successfully added to UploadQueue
         Assertions.assertNotEquals(avm.getArtistSongs(), new String[][]{});
     }
 
+    /**
+     * Tests that artist name is successfully returned by getArtistName()
+     */
     @Test
     public void testGetArtistName(){
         Assertions.assertEquals(avm.getArtistName(), "Metrofolk");
